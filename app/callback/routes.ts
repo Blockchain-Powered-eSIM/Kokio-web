8679f97580cb864d7658a0a4871d6fd914ca421f
Tanmay Goel <141454816+GuyPhy@users.noreply.github.com>
2026-05-23 23:34:50 +0530

    Delete app/callback/routes.ts

--- app/callback/routes.ts
+++
@@ -1,9 +0,0 @@
-// app/callback/route.ts
-import { NextRequest, NextResponse } from "next/server";
-
-const APP_SCHEME = "app.kokio.mobile";
-
-export function GET(request: NextRequest) {
-  const params = request.nextUrl.searchParams.toString();
-  return NextResponse.redirect(`${APP_SCHEME}://callback?${params}`, 302);
-}
