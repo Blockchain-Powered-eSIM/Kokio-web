3ab267e6e8fc88ea4679ddb2be4007aa6ca70582
Tanmay Goel <141454816+GuyPhy@users.noreply.github.com>
2026-05-23 21:47:30 +0530

    Prettier Fix

--- app/callback/routes.ts
+++ app/callback/routes.ts
@@ -1,12 +1,9 @@
 // app/callback/route.ts
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-const APP_SCHEME = 'app.kokio.mobile';
+const APP_SCHEME = "app.kokio.mobile";
 
 export function GET(request: NextRequest) {
   const params = request.nextUrl.searchParams.toString();
-  return NextResponse.redirect(
-    `${APP_SCHEME}://callback?${params}`,
-    302,
-  );
+  return NextResponse.redirect(`${APP_SCHEME}://callback?${params}`, 302);
 }
